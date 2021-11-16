ad955226cdbabdfa14d44cbebf4ae3542fedb24d
Luke Mino-Altherr <lminoaltherr@gmail.com>
2021-11-15 20:15:25 -0500

    Remove purgecss for now.
    This was breaking in the build process. Fix later.

--- gridsome.config.js
+++ gridsome.config.js
@@ -10,8 +10,6 @@ const postcssPlugins = [
   tailwind(),
 ]
 
-if (process.env.NODE_ENV === 'production') postcssPlugins.push(purgecss(require('./purgecss.config.js')))
-
 module.exports = {
   siteName: 'Min-Alt',
   siteUrl: 'https://luke-mino-altherr.github.io',
