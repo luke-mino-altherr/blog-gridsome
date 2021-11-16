1fd2dd85b933ea7232677fb3aefccce2fdccf75c
Luke Mino-Altherr <lminoaltherr@gmail.com>
2021-11-15 20:07:59 -0500

    Updates

---
+++ assets/js/page--src--pages--projects-vue.6ccfd3d1.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{fx0o:function(t,n,e){"use strict";e.r(n);var o={metaInfo:{title:"About us"}},s=e("KHd+"),u=Object(s.a)(o,(function(){var t=this.$createElement,n=this._self._c||t;return n("Layout",[n("p",[this._v("Projects coming soon...")])])}),[],!1,null,null,null);n.default=u.exports}}]);
