ae7331c036c31cf456503318c6022e9f3abe1ae2
Luke Mino-Altherr <lminoaltherr@gmail.com>
2021-11-26 23:04:07 -0500

    Remove h-screen.
    This was causing issues with sticky nav bar.

--- src/layouts/Content.vue
+++ src/layouts/Content.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-screen h-screen layout">
+  <div class="w-screen layout">
     <header class="sticky top-0 bg-white">
       <div class="h-1"></div>
       <NavBarContent />
